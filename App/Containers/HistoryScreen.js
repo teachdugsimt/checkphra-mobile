6748dfa091b9ad7489d689317759e4404fbe17d1
Katanyoo Ubalee <ubalee.k@gmail.com>
2018-10-02 16:51:06 +0700

    show owner history

--- App/Containers/HistoryScreen.js
+++ App/Containers/HistoryScreen.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, View, FlatList, Image } from 'react-native'
+import { ScrollView, Text, View, FlatList, Image, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import LinearGradient from "react-native-linear-gradient";
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 import QuestionActions from '../Redux/QuestionRedux'
-
+import moment from 'moment'
+import 'moment/locale/th'
 // Styles
 import styles from './Styles/HistoryScreenStyle'
 import { Colors } from '../Themes';
@@ -16,6 +17,7 @@ class HistoryScreen extends Component {
   // }
 
   componentDidMount() {
+    moment.locale('th')
     this.props.getHistory()
   }
 
@@ -27,14 +29,41 @@ class HistoryScreen extends Component {
         <FlatList
           data={this.props.history}
           renderItem={({ item }) => {
-            // let date =
+            let date = moment.unix(item.created_at).format("DD MMM YYYY")
+            let status = 'รอตรวจ'
+            let color = 'orange'
+            if (item.status == 'success') {
+              status = 'ตรวจแล้ว'
+              color = 'green'
+            }
             return (
-            <View style={{ height: 80, backgroundColor:'#ffffff33', marginTop: 1 }}>
+            <TouchableOpacity>
+            <View style={{ height: 80, backgroundColor:'#ffffffdd', marginTop: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
               <Image source={{ uri: 'https://s3-ap-southeast-1.amazonaws.com/checkphra/images/thumbs/tmb_100x100_'+item.images[0]}} style={{ width: 60, height: 60, margin: 10, borderRadius: 10 }}/>
-              <Text>{}</Text>
-            </View>)}}
+              <Text style={{
+                fontFamily: 'Prompt-SemiBold',
+                fontSize: 18,
+                color: Colors.brownText,
+                margin: 20
+              }}>{date}</Text>
+
+              <Text style={{
+                fontFamily: 'Prompt-SemiBold',
+                fontSize: 15,
+                color: 'white',
+                margin: 20,
+                paddingHorizontal: 20,
+                paddingTop: 5,
+                borderRadius: 15,
+                height: 30,
+                backgroundColor: color
+              }}>{status}</Text>
+            </View>
+            </TouchableOpacity>
+            )}}
         />
       </LinearGradient>
+
     )
   }
 }

37d0398548d69870bd75d7a3e629fcafef93f719
Katanyoo Ubalee <ubalee.k@gmail.com>
2018-11-15 11:22:21 +0700

    fix upload

--- App/Components/Picker2.js
+++ App/Components/Picker2.js
@@ -11,141 +11,141 @@ import { database } from 'react-native-firebase';
 
 var ImagePicker = require('react-native-image-picker');
 var options = {
-    title: 'Select Avatar',
-    customButtons: [
-        { name: 'fb', title: 'Choose Photo from Facebook' },
-    ],
-    storageOptions: {
-        skipBackup: true,
-        path: 'images'
-    }
+  title: 'Select Avatar',
+  customButtons: [
+    { name: 'fb', title: 'Choose Photo from Facebook' },
+  ],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
 };
 
 class Picker2 extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            avatarSource: null,
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      avatarSource: null,
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        // if (nextProps.imgaes != this.props.images) {
+  componentWillReceiveProps(nextProps) {
+    // if (nextProps.imgaes != this.props.images) {
 
-        //     if (nextProps.images[this.props.id]) {
-        //         // console.log('ADD IMAGE SUCCESS')
+    //     if (nextProps.images[this.props.id]) {
+    //         // console.log('ADD IMAGE SUCCESS')
 
-        //     }
-        // }
-    }
+    //     }
+    // }
+  }
 
-    componentWillMount() {
-        // this.props.clearImage()
-    }
+  componentWillMount() {
+    // this.props.clearImage()
+  }
 
-    pick = () => {
-        ImagePicker.showImagePicker(options, (response) => {
-            console.log('Response = ', response);
+  pick = () => {
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
 
-            if (response.didCancel) {
-                console.log('User cancelled image picker');
-            }
-            else if (response.error) {
-                console.log('ImagePicker Error: ', response.error);
-            }
-            else if (response.customButton) {
-                console.log('User tapped custom button: ', response.customButton);
-            }
-            else {
-                let source = { uri: response.uri };
-                console.log(response)
-                // You can also display the image using data:
-                // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+        console.log(response)
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
 
-                this.setState({
-                    avatarSource: source
-                });
-
-                // this.props.setImage({
-                //     uri: response.uri,
-                //     type: response.type,
-                //     name: response.fileName
-                // })
-
-                ImageResizer.createResizedImage(response.uri, 1024, 1024, 'JPEG', 0, 0, null)
-                .then((response) => {
-                  // response.uri is the URI of the new image that can now be displayed, uploaded...
-                  // response.path is the path of the new image
-                  // response.name is the name of the new image with the extension
-                  // response.size is the size of the new image
-                  console.log(response)
-                  this.props.setImage({
-                    uri: response.uri,
-                    type: 'image/jpeg',
-                    // name: response.fileName,
-                    name: response.name,
-                    // size: response.fileSize,
-                    size: response.size,
-                    tmp_name: response.path
-                  })
-                  console.log(response)
-                }).catch((err) => {
-                  // Oops, something went wrong. Check that the filename is correct and
-                  // inspect err to get more details.
-                  console.log(err)
-                });
-            }
+        this.setState({
+          avatarSource: source
         });
-    }
-    render() {
-        
-        return (
-            <View style={styles.container}>
-                <TouchableOpacity style={{ flex: 1 }} onPress={this.pick}>
-                    <View style={styles.uploadBox}>
-                        <Icon
-                            name="camera"
-                            size={40}
-                            color={Colors.brownTextTran}
-                        />
-                        <View style={{ flexDirection: 'row' }}>
-                            <Text style={styles.uploadBoxText}>สลิปการโอนเงิน</Text>
-                            {this.props.image && < Icon
-                                style={{ marginLeft: 33 }}
-                                name="squared-cross"
-                                size={24}
-                                color={'red'}
-                                onPress={() => { this.props.deleteImage() }}
-                            />
-                            }
-                        </View>
 
-                        {/* <Image source={this.state.avatarSource && this.props.image ? this.state.avatarSource : ''} style={{ width: '100%', height: '100%' }} /> */}
-                        <Image source={this.state.avatarSource} style={{ width: '100%', height: '100%' }} />
-                    </View>
-                </TouchableOpacity>
+        // this.props.setImage({
+        //     uri: response.uri,
+        //     type: response.type,
+        //     name: response.fileName
+        // })
 
+        ImageResizer.createResizedImage(response.uri, 1024, 1024, 'JPEG', 100, 0, null)
+          .then((response) => {
+            // response.uri is the URI of the new image that can now be displayed, uploaded...
+            // response.path is the path of the new image
+            // response.name is the name of the new image with the extension
+            // response.size is the size of the new image
+            console.log(response)
+            this.props.setImage({
+              uri: response.uri,
+              type: 'image/jpeg',
+              // name: response.fileName,
+              name: response.name,
+              // size: response.fileSize,
+              size: response.size,
+              tmp_name: response.path
+            })
+            console.log(response)
+          }).catch((err) => {
+            // Oops, something went wrong. Check that the filename is correct and
+            // inspect err to get more details.
+            console.log(err)
+          });
+      }
+    });
+  }
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={{ flex: 1 }} onPress={this.pick}>
+          <View style={styles.uploadBox}>
+            <Icon
+              name="camera"
+              size={40}
+              color={Colors.brownTextTran}
+            />
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={styles.uploadBoxText}>สลิปการโอนเงิน</Text>
+              {this.props.image && < Icon
+                style={{ marginLeft: 33 }}
+                name="squared-cross"
+                size={24}
+                color={'red'}
+                onPress={() => { this.props.deleteImage() }}
+              />
+              }
             </View>
-        )
-    }
+
+            {/* <Image source={this.state.avatarSource && this.props.image ? this.state.avatarSource : ''} style={{ width: '100%', height: '100%' }} /> */}
+            <Image source={this.state.avatarSource} style={{ width: '100%', height: '100%' }} />
+          </View>
+        </TouchableOpacity>
+
+      </View>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        image: state.payment.img_slip,
+  return {
+    image: state.payment.img_slip,
 
-    };
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        setImage: (source) => dispatch(PaymentActions.setImage(source)),
-        deleteImage: () => dispatch(PaymentActions.deleteImage()),
-    };
+  return {
+    setImage: (source) => dispatch(PaymentActions.setImage(source)),
+    deleteImage: () => dispatch(PaymentActions.deleteImage()),
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Picker2);
--- App/Services/PromotionApi.js
+++ App/Services/PromotionApi.js
@@ -4,8 +4,8 @@ let b
 if (process.env.NODE_ENV === 'production') {
   b = 'https://infiltech.org/checkphra-api/web/index.php/v1/'
 } else {
-  // b = 'https://infiltech.org/checkphra-api/web/index.php/v1/'   //true
-  b = 'http://192.168.1.45/CheckPhraApi/web/index.php/v1/'
+  b = 'https://infiltech.org/checkphra-api/web/index.php/v1/'   //true
+  // b = 'http://192.168.1.45/CheckPhraApi/web/index.php/v1/'
 }
 
 const create = (baseURL = b) => {
